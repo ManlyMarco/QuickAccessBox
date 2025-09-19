f634223b21895b5f00395a7b50e1bcbcce6b0c6a
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-09-19 20:55:04 +0200

    Move to sdk style projects

--- Shared_QuickAccessBox/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyDescription("Plugin for quickly searching through studio items in games by Illusion")]
-[assembly: AssemblyCompany("https://github.com/ManlyMarco/QuickAccessBox")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTitle(KK_QuickAccessBox.QuickAccessBox.GUID)]
-[assembly: AssemblyProduct(KK_QuickAccessBox.QuickAccessBox.GUID)]
-[assembly: AssemblyVersion(KK_QuickAccessBox.QuickAccessBox.Version)]
